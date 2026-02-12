b7a04ae385027d41cb99329a34302240b3f6709f
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-12 17:04:27 +0300

    Implement basic functionality to calculate and display factors of a user-defined number

--- FactorFinder.cpp
+++ FactorFinder.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+    // Basic functionality
+
+    // Accept Number To Calculate The Factors
+    int num;
+    cout << "Enter the number you want the factor of: \n";
+    cin >> num;
+
+    // Display The Output
+    for(int j=2;j<=num;j++){
+    cout << "\nThe factors of " << j << ": ";
+    for (int i = 2; i <= j/2; i++)
+    {
+
+        if (j % i == 0)
+            cout << i << " ";
+    }
+    }
+
 }
